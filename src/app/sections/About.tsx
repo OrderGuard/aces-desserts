56db6b97336f3faf5f908672e30e24dfbbdd8fa4
dm <jmrcas09@gmail.com>
2025-08-22 05:50:45 +0800

    About - update image to video

--- src/app/sections/About.tsx
+++ src/app/sections/About.tsx
@@ -14,16 +14,20 @@ export default function About() {
             data-aos-delay="100"
           >
             <div className="about-img">
-              <Image src={aboutImage} alt="" />
+              {/*<Image src={aboutImage} alt="" />*/}
+              <video autoPlay muted loop playsInline>
+                <source src="/assets/videos/kk83-video.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
             </div>
+
           </div>
           <div className="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content">
             <h2>
               About Us
             </h2>
-{/*<p className="italic leading-relaxed tracking-wide my-6 px-4 text-lg text-gray-700">*/}
-<p className="hero-text">
-
+            {/*<p className="italic leading-relaxed tracking-wide my-6 px-4 text-lg text-gray-700">*/}
+            <p className="hero-text">
               Klub Kitchen 83 is your go-to in Sketty,
               Swansea for bold street food flavours — from loaded fries and tacos to our famous burgers.
               Freshly made, packed with flavour, and served with a smile.
