bca1634ffcc8d81866386fe7df47645db63595fe
dm <jmrcas09@gmail.com>
2025-08-22 07:20:58 +0800

    Sections - use data instead api

--- src/app/sections/Specials.tsx
+++ src/app/sections/Specials.tsx
@@ -10,16 +10,69 @@ import { specialsFilters } from '../data/data';
 export default function Specials() {
   const [data, setData] = useState<any | []>([]);
   const [items, setItems] = useState<any | []>([]);
+  const specials = [
+    {
+      id: 1,
+      image: './assets/images/specials/specials-1.png',
+      title: 'Architecto ut aperiam autem id',
+      subtitle:
+        'Qui laudantium consequatur laborum sit qui ad sapiente dila parde sonata raqer a videna mareta paulona marka',
+      content:
+        'Et nobis maiores eius. Voluptatibus ut enim blanditiis atque harum sint. Laborum eos ipsum ipsa odit magni. Incidunt hic ut molestiae aut qui. Est repellat minima eveniet eius et quis magni nihil. Consequatur dolorem quaerat quos qui similique accusamus nostrum rem vero',
+      active: true,
+    },
+    {
+      id: 2,
+      image: './assets/images/specials/specials-2.png',
+      title: 'Et blanditiis nemo veritatis excepturi',
+      subtitle:
+        'Qui laudantium consequatur laborum sit qui ad sapiente dila parde sonata raqer a videna mareta paulona marka',
+      content:
+        'Ea ipsum voluptatem consequatur quis est. Illum error ullam omnis quia et reiciendis sunt sunt est. Non aliquid repellendus itaque accusamus eius et velit ipsa voluptates. Optio nesciunt eaque beatae accusamus lerode pakto madirna desera vafle de nideran pal',
+      active: false,
+    },
+    {
+      id: 3,
+      image: './assets/images/specials/specials-3.png',
+      title: 'Impedit facilis occaecati odio neque aperiam sit',
+      subtitle:
+        'Eos voluptatibus quo. Odio similique illum id quidem non enim fuga. Qui natus non sunt dicta dolor et. In asperiores velit quaerat perferendis aut',
+      content:
+        'Iure officiis odit rerum. Harum sequi eum illum corrupti culpa veritatis quisquam. Neque necessitatibus illo rerum eum ut. Commodi ipsam minima molestiae sed laboriosam a iste odio. Earum odit nesciunt fugiat sit ullam. Soluta et harum voluptatem optio quae',
+      active: false,
+    },
+    {
+      id: 4,
+      image: './assets/images/specials/specials-4.png',
+      title:
+        'Fuga dolores inventore laboriosam ut est accusamus laboriosam dolore',
+      subtitle:
+        'Totam aperiam accusamus. Repellat consequuntur iure voluptas iure porro quis delectus',
+      content:
+        'Eaque consequuntur consequuntur libero expedita in voluptas. Nostrum ipsam necessitatibus aliquam fugiat debitis quis velit. Eum ex maxime error in consequatur corporis atque. Eligendi asperiores sed qui veritatis aperiam quia a laborum inventore',
+      active: false,
+    },
+    {
+      id: 5,
+      image: './assets/images/specials/specials-5.png',
+      title: 'Est eveniet ipsam sindera pad rone matrelat sando reda',
+      subtitle: 'Omnis blanditiis saepe eos autem qui sunt debitis porro quia.',
+      content:
+        'Exercitationem nostrum omnis. Ut reiciendis repudiandae minus. Omnis recusandae ut non quam ut quod eius qui. Ipsum quia odit vero atque qui quibusdam amet. Occaecati sed est sint aut vitae molestiae voluptate vel',
+      active: false,
+    },
+  ];
 
-  const getSpecialsData = () => {
-    fetch('http://localhost:3000/api/specials')
-      .then(res => res.json())
-      .then(data => setData(data))
-      .catch(e => console.log(e.message));
-  };
+  //const getSpecialsData = () => {
+    //fetch('http://localhost:3000/api/specials')
+      //.then(res => res.json())
+      //.then(data => setData(data))
+      //.catch(e => console.log(e.message));
+  //};
 
   useEffect(() => {
-    getSpecialsData();
+    setData(specials)
+    //getSpecialsData();
   }, []);
 
   useEffect(() => {
