cf1587df6289c62c21578dc4277050104fde713f
dm <jmrcas09@gmail.com>
2025-08-22 03:21:31 +0800

    Fix script issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 // import Icons
 import 'bootstrap-icons/font/bootstrap-icons.css';
+// Script
+import Script from "next/script";
 
 // import bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
@@ -45,11 +47,12 @@ export default function RootLayout({
         {children}
         <Footer />
         <BackToTopBtn />
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
           crossOrigin="anonymous"
-        ></script>
+          strategy="afterInteractive"   // ✅ safe for Bootstrap JS
+        />
       </body>
     </html>
   );
